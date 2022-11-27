ae1e4a2887451fc87c6bb42a2c643a1e7b25fa47
venky0199 <114491905+venky0199@users.noreply.github.com>
2022-11-27 17:37:00 +0530

    Add files via upload

---
+++ innerhtml.js
@@ -0,0 +1,11 @@
+function change()
+{
+   // document.getElementById("changetext").innerHTML="This is an changed text";
+
+   document.getElementById("styling").innerHTML="this is an changed paragraph!!";
+   document.getElementById("styling").style.backgroundColor="red";
+   document.getElementById("styling").style.textAlign="center";
+   document.getElementById("styling").style
+   .color="white";
+   document.getElementById("styling").style.border="3px solid blue";
+}
